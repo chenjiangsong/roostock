bee560eca8b97cb87daa473600258ce349d61fab
chenjiangsong <274815408@qq.com>
2016-08-22 21:01:04 +0800

    add logback

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,4 +6,14 @@ gulp.task('default', () => {
   console.log(webpack.a);
 })
 
-  
+
+function logback(err, stats) {
+    // console.log(stats.toString());
+    if (stats.hasErrors() || stats.hasWarnings()) {
+        console.log('\n==============================');
+        console.log(stats.compilation.errors.toString() || stats.compilation.warnings.toString());
+    } else {
+        console.log('--------------------------------------');
+        console.log('webpack success at %s', new Date(stats.endTime).toLocaleString());
+    }
+}
